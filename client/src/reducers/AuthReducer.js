f702cd407d6f38994988cc86eac2dc091b548569
Leqi Long <leqilong@Leqis-MacBook-Pro.local>
2019-06-04 01:44:11 -0700

    Finished Google Auth

--- client/src/reducers/AuthReducer.js
+++ client/src/reducers/AuthReducer.js
@@ -1,13 +1,16 @@
+import {SIGN_IN, SIGN_OUT} from '../actions/types';
 const INITIAL_STATE = {
-  isSignedIn: null
+  isSignedIn: null,
+  userId: null
 }
 export default (state = INITIAL_STATE, action) => {
   switch(action.type){
-    case 'SIGN_IN':
+    case SIGN_IN:
       //take the state object out and put it into an new object using '...' syntax, then update the isSignedIn value
-      return {...state, isSignedIn: true}
-    case 'SIGN_OUT':
-      return {...state, isSignedIn: false}
+      return {...state, isSignedIn: true, userId: action.payload}
+    case SIGN_OUT:
+      //when a user signs out, we should clear the userId property
+      return {...state, isSignedIn: false, userId: null}
     default:
       return state;
   }

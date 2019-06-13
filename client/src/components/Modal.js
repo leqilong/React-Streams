e030ef3fa9cbfbeff04fed369567e18da7ef7cf6
Leqi Long <leqilong@Leqis-MacBook-Pro.local>
2019-06-13 01:58:28 -0700

    Finished DeleteStream component

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
 const Modal = props => {
+  //the outter div with "dimmer" classname is the outer part of the modal
+  //when the dimmer class is clicked on, the modal should exit
   return ReactDOM.createPortal(
-    <div className="ui dimmer modals visible active">
-      <div className="ui standard modal visible active">
-        <div className="header">Delete Stream</div>
-        <div className="content">Are you sure you want to delete this stream?</div>
-        <div className="actions">
-          <button className="ui primary button">Delete</button>
-          <button className="ui button">Cancel</button>
-        </div>
+    <div onClick={props.onDismiss} className="ui dimmer modals visible active">
+      <div onClick={(e)=> e.stopPropagation()}className="ui standard modal visible active">
+        <div className="header">{props.title}</div>
+        <div className="content">{props.content}</div>
+        <div className="actions">{props.actions}</div>
       </div>
     </div>,
     document.querySelector('#modal')
--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,13 +1,53 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
 import Modal from '../Modal';
+import history from '../../history';
+import {fetchStream, deleteStream} from '../../actions';
 
-const StreamDelete = ()=> {
-  return(
-    <div>
-      StreamDelete
-      <Modal />
-    </div>
-  )
+class StreamDelete extends React.Component {
+
+  componentDidMount(){
+    //the id in props are passed down from defined Route to this component in App.js
+    this.props.fetchStream(this.props.match.params.id);
+  }
+  //the buttons tag are wrapped in a dig because it's not correct JS syntax to assign multiple jsx to a variable
+  renderActions(){
+    const {id} = this.props.match.params;
+    return (
+      <React.Fragment>
+        <button onClick={()=> this.props.deleteStream(id)}className="ui button negative">Delete</button>
+        <Link to="/" className="ui button">Cancel</Link>
+      </React.Fragment>
+    );
+  }
+
+  renderContent(){
+    if(!this.props.stream){
+      return 'Are you sure you want to delete this stream?'
+    }
+
+    return `Are you sure you want to delete this stream with title: ${this.props.stream.title}?`
+  }
+
+  render(){
+    return(
+        <Modal
+          title="Delete Stream"
+          content={this.renderContent()}
+          actions={this.renderActions()}
+          onDismiss={()=> history.push('/')}
+        />
+    )
+  };
 }
 
-export default StreamDelete;
+//calling state out of the redux store
+//the state is empty until we call fetchStream in componentDidMount
+const mapStateToProps = (state, ownProps) => {
+  return {
+    stream: state.streams[ownProps.match.params.id]
+  };
+}
+
+export default connect(mapStateToProps, {fetchStream, deleteStream})(StreamDelete);
